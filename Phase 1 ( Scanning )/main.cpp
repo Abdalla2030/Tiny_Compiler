d56e7cb26e05ab5fdcfb91aacc74ce28672ae9ef
Abdalla Fadl <abdalla.fadle2001@gmail.com>
2022-12-09 21:45:52 +0200

    first

---
+++ Phase 1 ( Scanning )/main.cpp
@@ -0,0 +1,333 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <string>
+using namespace std;
+
+/*
+{ Sample program
+  in TINY language
+  compute factorial
+}
+
+read x; {input an integer}
+if 0<x then {compute only if x>=1}
+  fact:=1;
+  repeat
+    fact := fact * x;
+    x:=x-1
+  until x=0;
+  write fact {output factorial}
+end
+*/
+
+/// sequence of statements separated by ;
+/// no procedures - no declarations
+/// all variables are integers
+/// variables are declared simply by assigning values to them :=
+/// if-statement: if (boolean) then [else] end
+/// repeat-statement: repeat until (boolean)
+/// boolean only in if and repeat conditions < = and two mathematical expressions
+/// math expressions integers only, + - * / ^
+/// I/O read write
+/// Comments {}
+
+////////////////////////////////////////////////////////////////////////////////////
+/// Strings /////////////////////////////////////////////////////////////////////////
+
+bool Equals(const char* a, const char* b)
+{
+    return strcmp(a, b)==0;
+}
+
+bool StartsWith(const char* a, const char* b)
+{
+    int nb=strlen(b);
+    return strncmp(a, b, nb)==0;
+}
+
+void Copy(char* a, const char* b, int n=0)
+{
+    if(n>0) {strncpy(a, b, n); a[n]=0;}
+    else strcpy(a, b);
+}
+
+void AllocateAndCopy(char** a, const char* b)
+{
+    if(b==0) {*a=0; return;}
+    int n=strlen(b);
+    *a=new char[n+1];
+    strcpy(*a, b);
+}
+
+////////////////////////////////////////////////////////////////////////////////////
+/// Input and Output ////////////////////////////////////////////////////////////////
+
+#define MAX_LINE_LENGTH 10000
+
+struct InFile
+{
+    FILE* file;
+    int cur_line_num;
+
+    char line_buf[MAX_LINE_LENGTH];
+    int cur_ind, cur_line_size;
+
+    InFile(const char* str) {file=0; if(str) file=fopen(str, "r"); cur_line_size=0; cur_ind=0; cur_line_num=0;}
+    ~InFile(){if(file) fclose(file);}
+
+    void SkipSpaces()
+    {
+        while(cur_ind<cur_line_size)
+        {
+            char ch=line_buf[cur_ind];
+            if(ch!=' ' && ch!='\t' && ch!='\r' && ch!='\n') break;
+            cur_ind++;
+        }
+    }
+
+    bool SkipUpto(const char* str)
+    {
+        while(true)
+        {
+            SkipSpaces();
+            while(cur_ind>=cur_line_size) {if(!GetNewLine()) return false; SkipSpaces();}
+
+            if(StartsWith(&line_buf[cur_ind], str))
+            {
+                cur_ind+=strlen(str);
+                return true;
+            }
+            cur_ind++;
+        }
+        return false;
+    }
+
+    bool GetNewLine()
+    {
+        cur_ind=0; line_buf[0]=0;
+        if(!fgets(line_buf, MAX_LINE_LENGTH, file)) return false;
+        cur_line_size=strlen(line_buf);
+        if(cur_line_size==0) return false; // End of file
+        cur_line_num++;
+        return true;
+    }
+
+    char* GetNextTokenStr()
+    {
+        SkipSpaces();
+        while(cur_ind>=cur_line_size) {if(!GetNewLine()) return 0; SkipSpaces();}
+        return &line_buf[cur_ind];
+    }
+
+    void Advance(int num)
+    {
+        cur_ind+=num;
+    }
+};
+
+struct OutFile
+{
+    FILE* file;
+    OutFile(const char* str) {file=0; if(str) file=fopen(str, "w");}
+    ~OutFile(){if(file) fclose(file);}
+
+    void Out(const char* s)
+    {
+        fprintf(file, "%s\n", s); fflush(file);
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+/// Compiler Parameters /////////////////////////////////////////////////////////////
+
+struct CompilerInfo
+{
+    InFile in_file;
+    OutFile out_file;
+    OutFile debug_file;
+
+    CompilerInfo(const char* in_str, const char* out_str, const char* debug_str)
+                : in_file(in_str), out_file(out_str), debug_file(debug_str)
+    {
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////////
+/// Scanner /////////////////////////////////////////////////////////////////////////
+
+#define MAX_TOKEN_LEN 40
+
+enum TokenType{
+                IF, THEN, ELSE, END, REPEAT, UNTIL, READ, WRITE,TYPES,
+                ASSIGN, EQUAL, LESS_THAN,
+                PLUS, MINUS, TIMES, DIVIDE, POWER,
+                SEMI_COLON,
+                LEFT_PAREN, RIGHT_PAREN,
+                LEFT_BRACE, RIGHT_BRACE,
+                ID, NUM,
+                ENDFILE, ERROR,
+                INT,BOOL,REAL
+              };
+
+/// Used for debugging only /////////////////////////////////////////////////////////
+const char* TokenTypeStr[]=
+            {
+                "If", "Then", "Else", "End", "Repeat", "Until", "Read", "Write", "Types",
+                "Assign", "Equal", "LessThan",
+                "Plus", "Minus", "Times", "Divide", "Power",
+                "SemiColon",
+                "LeftParen", "RightParen",
+                "LeftBrace", "RightBrace",
+                "ID", "Num",
+                "EndFile", "Error",
+                "Int","Bool","Real"
+            };
+
+struct Token
+{
+    TokenType type;
+    char str[MAX_TOKEN_LEN+1];
+
+    Token(){str[0]=0; type=ERROR;}
+    Token(TokenType _type, const char* _str) {type=_type; Copy(str, _str);}
+};
+
+const Token reserved_words[]=
+{
+    Token(IF, "if"),
+    Token(THEN, "then"),
+    Token(ELSE, "else"),
+    Token(END, "end"),
+    Token(REPEAT, "repeat"),
+    Token(UNTIL, "until"),
+    Token(READ, "read"),
+    Token(WRITE, "write"),
+    Token(TYPES, "types"),
+    Token(INT, "int"),
+    Token(BOOL, "bool"),
+    Token(REAL, "real")
+};
+const int num_reserved_words=sizeof(reserved_words)/sizeof(reserved_words[0]);
+
+// if there is tokens like < <=, sort them such that sub-tokens come last: <= <
+// the closing comment should come immediately after opening comment
+const Token symbolic_tokens[]=
+{
+    Token(ASSIGN, ":="),
+    Token(EQUAL, "="),
+    Token(LESS_THAN, "<"),
+    Token(PLUS, "+"),
+    Token(MINUS, "-"),
+    Token(TIMES, "*"),
+    Token(DIVIDE, "/"),
+    Token(POWER, "^"),
+    Token(SEMI_COLON, ";"),
+    Token(LEFT_PAREN, "("),
+    Token(RIGHT_PAREN, ")"),
+    Token(LEFT_BRACE, "{"),
+    Token(RIGHT_BRACE, "}")
+};
+const int num_symbolic_tokens=sizeof(symbolic_tokens)/sizeof(symbolic_tokens[0]);
+
+inline bool IsDigit(char ch){return (ch>='0' && ch<='9');}
+inline bool IsLetter(char ch){return ((ch>='a' && ch<='z') || (ch>='A' && ch<='Z'));}
+inline bool IsLetterOrUnderscore(char ch){return (IsLetter(ch) || ch=='_');}
+/////////////////////////////////////////////////////////////////////////////
+void GetNextToken(CompilerInfo* pci, Token* ptoken)
+{
+    ptoken->type=ERROR;
+    ptoken->str[0]=0;
+
+    int i;
+    char* s=pci->in_file.GetNextTokenStr();
+    if(!s)
+    {
+        ptoken->type=ENDFILE;
+        ptoken->str[0]=0;
+        return;
+    }
+
+    for(i=0;i<num_symbolic_tokens;i++)
+    {
+        if(StartsWith(s, symbolic_tokens[i].str))
+            break;
+    }
+
+    if(i<num_symbolic_tokens)
+    {
+        if(symbolic_tokens[i].type==LEFT_BRACE)
+        {
+            pci->in_file.Advance(strlen(symbolic_tokens[i].str));
+            if(!pci->in_file.SkipUpto(symbolic_tokens[i+1].str)) return;
+            return GetNextToken(pci, ptoken);
+        }
+        ptoken->type=symbolic_tokens[i].type;
+        Copy(ptoken->str, symbolic_tokens[i].str);
+    }
+    else if(IsDigit(s[0]))
+    {
+        int j=1;
+        while(IsDigit(s[j])) j++;
+
+        ptoken->type=NUM;
+        Copy(ptoken->str, s, j);
+    }
+    else if(IsLetterOrUnderscore(s[0]))
+    {
+        int j=1;
+        while(IsLetterOrUnderscore(s[j])) j++;
+
+        ptoken->type=ID;
+        Copy(ptoken->str, s, j);
+
+        for(i=0;i<num_reserved_words;i++)
+        {
+            if(Equals(ptoken->str, reserved_words[i].str))
+            {
+                ptoken->type=reserved_words[i].type;
+                break;
+            }
+        }
+    }else if(ptoken->type=ERROR){
+            Copy(ptoken->str, s,1);
+    }
+    int len=strlen(ptoken->str);
+    if(len>0) pci->in_file.Advance(len);
+}
+/////////////////////////////////////////////////////////////////////////////
+void StartScanning(CompilerInfo* CompilerObj){
+
+    Token TokenObj;
+
+    cout<<("========== Scanning ==========")<<endl;
+    cout<<endl;
+    CompilerObj->out_file.Out("========== Scanning ==========");
+    while(true){
+        GetNextToken(CompilerObj,&TokenObj);
+        cout<<"["<<CompilerObj->in_file.cur_line_num<<"] "<<TokenObj.str<<" ["<<TokenTypeStr[TokenObj.type]<<"]";
+
+        // write in file
+        string temp_str="["+to_string(CompilerObj->in_file.cur_line_num)+"] "+TokenObj.str+" ["+TokenTypeStr[TokenObj.type]+"]"; //converting number to a string
+        char const* char_array= temp_str.c_str(); //converting string to char
+        CompilerObj->out_file.Out(char_array);
+
+        cout<<endl;
+        if(TokenObj.type==ENDFILE){   // stop scanning
+            cout<<endl;
+            cout<<("=============================");
+            CompilerObj->out_file.Out("=============================");
+            break;
+        }
+    }
+}
+/////////////////////////////////////////////////////////////////////////////
+int main(){
+
+    CompilerInfo compiler_info("input.txt", "output.txt", "debug.txt");
+    StartScanning(&compiler_info);
+
+    return 0;
+}
+////////////////////////////////////////////////////////////////////////////////
